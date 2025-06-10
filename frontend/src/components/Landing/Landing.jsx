c0494b692dece5f7c590bb0c3c339441a2414520
alanreji2004 <alenreji2004@gmail.com>
2025-06-10 20:21:21 +0530

    added user name if loggedin in landing page

--- frontend/src/components/Landing/Landing.jsx
+++ frontend/src/components/Landing/Landing.jsx
@@ -1,18 +1,61 @@
-import React from 'react'
+import {React,useState,useEffect} from 'react'
 import styles from './Landing.module.css'
 import { Link, useNavigate } from "react-router-dom"
+import { getAuth, onAuthStateChanged, signOut } from 'firebase/auth';
+import { doc, getDoc } from 'firebase/firestore';
+import { db, app } from '../../firebase';
+import person from '../../assets/person.png'
 
 const Landing = () => {
     const navigate = useNavigate()
+    const auth = getAuth(app);
+    const [user,setUser] = useState(null);
+    const [photo,setPhoto] = useState(null);
+    const [name,setName] = useState(null);
+    const [loading,setLoading] = useState(false);
+
     const handleGetStarted = () => {
-        navigate('/home')
+        navigate('/home');
     }
+
+    const handleToProfile = () => {
+        navigate('/profile');
+    };
+    useEffect(() =>{
+        setLoading(true);
+        const unsubscribe = onAuthStateChanged(auth,async(currentUser)=>{
+            if(currentUser){
+                setUser(currentUser);
+                const userDocRef = doc(db,'users',currentUser.uid);
+                const userDoc = await getDoc(userDocRef);
+                if(userDoc.exists()){
+                    const data = userDoc.data();
+                    if(data.photoURL){
+                        setPhoto(data.photoURL);
+                    }
+                    setName(data.name);
+                }
+            }
+            setLoading(false);
+        });
+        return () => unsubscribe();
+    },[auth,navigate]);
+
+
   return (
     <div className={styles.wrapper}>
+    {loading && <div className={styles.spinner}></div>}
       <nav className={styles.navbar}>
         <div className={styles.logo}>Verbio</div>
         <div className={styles.navButtons}>
-           <Link to="/login" className={styles.signIn}>Sign in</Link>
+            {user?<div className={styles.profile} onClick={handleToProfile}>
+                <div className={styles.profileIconDiv}>
+                    <img src={person} alt="" className={styles.profileIcon} />
+                    </div>
+                <div className={styles.userName}>{name}</div>
+            </div>
+         : <Link to="/login" className={styles.signIn}>Sign in</Link>
+        }   
           <button className={styles.getStarted} onClick={handleGetStarted}>Get Started</button>
         </div>
       </nav>
