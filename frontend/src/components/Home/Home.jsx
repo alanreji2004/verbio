34e1ed38830a858dfda19df50d910da58db524e9
alanreji2004 <alenreji2004@gmail.com>
2025-06-12 21:39:38 +0530

    home page

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -6,6 +6,7 @@ import { doc, getDoc } from 'firebase/firestore';
 import { db, app } from '../../firebase';
 import person from '../../assets/person.png'
 import write from '../../assets/write.png'
+import { FaHeart } from 'react-icons/fa'
 
 const Home = () => {
 
@@ -15,8 +16,9 @@ const Home = () => {
     const [photo,setPhoto] = useState(null);
     const [name,setName] = useState(null);
     const [loading,setLoading] = useState(false);
-
-
+    const [blogs, setBlogs] = useState([]);
+    const [blogLoading, setBlogLoading] = useState(true);
+    const backendApi = import.meta.env.VITE_BACKEND_API;
     const handleToProfile = () => {
         navigate('/profile');
     };
@@ -25,27 +27,63 @@ const Home = () => {
     navigate('/write-story');
     }
 
-    useEffect(() =>{
-        setLoading(true);
-        const unsubscribe = onAuthStateChanged(auth,async(currentUser)=>{
-            if(currentUser){
-                setUser(currentUser);
-                const userDocRef = doc(db,'users',currentUser.uid);
-                const userDoc = await getDoc(userDocRef);
-                if(userDoc.exists()){
-                    const data = userDoc.data();
-                    if(data.photoURL){
-                        setPhoto(data.photoURL);
-                    }
-                    setName(data.name);
-                }
-              }else{
-                navigate('/login');
+    useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
+      
+      if (!currentUser) {
+        navigate('/login');
+        return;
+      }
+
+      setUser(currentUser);
+
+      const fetchProfile = async () =>{ 
+        
+      const userDocRef = doc(db,'users',currentUser.uid);
+        const userDoc = await getDoc(userDocRef);
+          if(userDoc.exists()){
+            const data = userDoc.data();
+              if(data.photoURL){
+                setPhoto(data.photoURL);
+                  }
+                setName(data.name);
               }
-            setLoading(false);
-        });
-        return () => unsubscribe();
-    },[auth,navigate]);
+          };
+
+      const fetchBlogs = async () =>{
+        try{
+          const res = await fetch(`${backendApi}/allblogs/`);
+
+          const data = await res.json();
+          const formattedBlogs = data.map(blog => {
+            if (blog.createdAt && blog.createdAt._seconds) {
+                 blog.createdDate = new Date(blog.createdAt._seconds * 1000);
+            } else {
+              blog.createdDate = null;
+            }
+            return blog;
+          });
+
+          setBlogs(formattedBlogs);
+          }catch (err) {
+            console.error('Error fetching blogs:', err);
+          }finally {
+        setBlogLoading(false);
+        }
+      };
+
+      setLoading(true);
+      await Promise.all([fetchProfile(),fetchBlogs()]);
+      setLoading(false);  
+    });
+    return () => unsubscribe();
+  }, [auth, navigate]);
+
+  const stripHtml = (html) => {
+    const div = document.createElement('div');
+    div.innerHTML = html;
+    return div.textContent || div.innerText || '';
+  };
 
   return (
     <div className={styles.mainDiv}>
@@ -64,6 +102,48 @@ const Home = () => {
           </div>
         </div>
       </nav>
+      <div className={styles.contentDiv}>
+        <div className={styles.firstHeading}>Suggested for {name}!</div>
+      <div className={styles.blogSection}>
+        {blogLoading?(
+          <div>Loading Blogs...</div>
+            ):(
+              blogs.length > 0?(
+                blogs.map((blog,index) => (
+                  <Link to={`/blog/${blog.id}`}
+                    key={blog.id}
+                      style={{ textDecoration: 'none',color:'inherit' }}
+                        >
+                        <div key={index} className={styles.eachBlog}>
+                          <div className={styles.blogTitle}>{blog.title}</div>
+                          <div className={styles.secondLine}>
+                          <div className={styles.nameAndDate}>
+                            <div className={styles.authorName}>{blog.authorName}</div>
+                            <div className={styles.dateSection}>
+                                <div className={styles.date}>
+                                    {blog.createdDate && blog.createdDate.toLocaleDateString('en-US', {
+                                        year  : 'numeric',
+                                        month: 'long',
+                                        day: 'numeric',
+                                     })}
+                                </div>                      
+                            </div>
+                          </div>
+                            <div className={styles.likeSection}>
+                              <FaHeart className={styles.likedHeart} />
+                              <div className={styles.likeCount}>{blog.likes}</div>
+                            </div>
+                          </div>
+                          <div className={styles.blogContent}>{stripHtml(blog.content)?.split(' ').slice(0, 18).join(' ')}...</div>
+                        </div>
+                        </Link>
+                      ))
+                    ):(
+                <div>No blogs to show...</div>
+              )
+            )}
+        </div>
+        </div>
       <div className={styles.notes}>
         <div className={styles.line1}></div>
         <div className={styles.line2}></div>
